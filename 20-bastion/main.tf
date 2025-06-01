cebaae5e99f965b114c96c92803c6495c417f37e
ganeshmoka <ganeshmoka1999@gmail.com>
2025-06-01 18:03:08 +0530

    terraform_eks

---
+++ 20-bastion/main.tf
@@ -0,0 +1,20 @@
+module "bastion" {
+  source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
+
+  name = local.resource_name
+  ami = data.aws_ami.joindevops.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id              = local.public_subnet_id
+  user_data = file("bastion.sh")
+  tags = merge(var.common_tags,var.bastion_tags,
+  {
+    Name = local.resource_name
+  }
+  )
+}
+
+
+
+
+
---
+++ 20-bastion/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }   
+}
+
+backend "s3" {
+    bucket         = "devgani-expense-dev"
+    key            = "expense_dev_bastion"
+    region         = "us-east-1"
+    dynamodb_table = "devgani-expense-dev"
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
