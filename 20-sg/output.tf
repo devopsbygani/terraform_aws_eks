1bac09dbf1eb7a7327dbbd5a9794587e4ada425b
ganeshmoka <ganeshmoka1999@gmail.com>
2025-06-01 16:38:26 +0530

    terraform_eks

---
+++ 20-sg/output.tf
@@ -0,0 +1,12 @@
+output "mysql_sg_id" {
+    value = module.mysql_sg.id
+}
+
+output "backend_sg_id" {
+    value = module.backend_sg.id
+}
+
+output "frontend_sg_id" {
+    value = module.frontend_sg.id
+}
+
