4d33e27c3b0272b7e55ff49124c0fa93076a950c
ganeshmoka <ganeshmoka1999@gmail.com>
2025-06-08 09:37:09 +0530

    terraform_eks

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -1,4 +1,4 @@
-module "alb" {
+module "ingress_alb" {
   source = "terraform-aws-modules/alb/aws"
   internal = false  
 
@@ -16,7 +16,7 @@ module "alb" {
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = module.alb.arn
+  load_balancer_arn = module.ingress_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -32,7 +32,7 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.alb.arn
+  load_balancer_arn = module.ingress_alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
@@ -58,10 +58,45 @@ module "records" {
       name    = "expense-${var.environment}" #expense-dev.devgani.online
       type    = "A"
       alias   = {
-        name    = module.alb.dns_name
-        zone_id = module.alb.zone_id   # This belongs ALB internal hosted zone, not ours
+        name    = module.ingress_alb.dns_name
+        zone_id = module.ingress_alb.zone_id   # This belongs ALB internal hosted zone, not ours
       }
       allow_overwrite = true
     },
   ]
+}
+
+resource "aws_lb_target_group" "expense" {
+  name     = local.resource_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    matcher = "200-299"
+    path = "/"
+    port = 80
+    protocol = "HTTP"
+    timeout = 4
+  }
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100 # low priority will be evaluated first
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.expense.arn
+  }
+
+  condition {
+    host_header {
+      values = ["expense-${var.environment}.${var.zone_name}"] #expense-dev.devgani.online
+    }
+  }
 }
