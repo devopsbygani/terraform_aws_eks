30b318034bcf0ba7df8f57644b2a201ec11346b0
ganeshmoka <ganeshmoka1999@gmail.com>
2025-06-18 21:42:55 +0530

    terraform

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,5 +1,6 @@
 module "aws_vpc" {
     source = "git::https://github.com/devopsbygani/terraform_aws_vpc.git?ref=main"
+    vpc_cidr = var.vpc_cidr
     project = var.project_name
     envirnoment = var.environment
     public_subnet_cidrs = var.public_subnet_cidrs
