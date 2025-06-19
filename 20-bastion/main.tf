ed74c8bc2c0f95a63a3c9a5485ab8684b9c2eb3d
ganeshmoka <ganeshmoka1999@gmail.com>
2025-06-19 21:28:15 +0530

    terraform_eks

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -2,8 +2,9 @@ module "bastion" {
   source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
 
   name = local.resource_name
-  ami = data.aws_ami.joindevops.id
-  instance_type          = "t2.micro"
+  #ami = data.aws_ami.joindevops.id
+  ami = ami-09c813fb71547fc4f
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
   user_data = file("bastion.sh")
@@ -16,5 +17,22 @@ module "bastion" {
 
 
 
+# module "bastion" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+  
+#   ami = data.aws_ami.joindevops.id
+#   name = local.resource_name
 
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [local.bastion_sg_id]
+#   subnet_id              = local.public_subnet_id
+#   user_data = file("bastion.sh")
+#   tags = merge(
+#     var.common_tags,
+#     var.bastion_tags,
+#     {
+#         Name = local.resource_name
+#     }
+#   )
+# }
 
