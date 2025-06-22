52791dfbe5e9f72e081d9dfc74d8349e19b60b33
ganeshmoka <ganeshmoka1999@gmail.com>
2025-06-22 18:13:29 +0530

    terraform_eks

---
+++ 70-ecr/parameters.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "backend_arn" {
+  name  = "/${var.project_name}/${var.environment}/backend_arn"
+  type  = "String"
+  value = aws_ecr_repository.backend.arn
+}
+
+resource "aws_ssm_parameter" "frontend_arn" {
+  name  = "/${var.project_name}/${var.environment}/frontend_arn"
+  type  = "String"
+  value = aws_ecr_repository.frontend.arn
+}
